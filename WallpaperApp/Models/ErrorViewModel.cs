07ed96294573bc1dd75a72235198a328bf5d9c96
mehmed <mehmed2kw@gmail.com>
2022-12-04 16:39:13 +0200

    Add project files.

---
+++ WallpaperApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WallpaperApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
